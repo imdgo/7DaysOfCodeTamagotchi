b1c3ec59101f52f0a776562afce7d82662ccf8e0
Diego Alves <diego.alves.santos93@gmail.com>
2024-06-21 00:42:54 -0300

    implementacao de tratamento da resposta da API do pokémon

---
+++ TamagotchiPokemon/PokemonDetailsResult.cs
@@ -0,0 +1,16 @@
+﻿public class PokemonDetailsResult
+{
+    public string Name { get; set; }
+    public List<AbilitiesDetail> Abilities { get; set; }
+
+    public class AbilitiesDetail
+    {
+        public Ability Ability { get; set; }
+
+    }
+    public class Ability
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
